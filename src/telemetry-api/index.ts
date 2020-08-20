4d95d41b20f2474db54b89319aeef354b5443106
Tony Ross <785009+antross@users.noreply.github.com>
2020-08-20 16:49:52 -0500

    Fix: Allow telemetry queue to be directly in request body

--- src/telemetry-api/index.ts
+++ src/telemetry-api/index.ts
@@ -3,17 +3,22 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { sendPendingData, addToAppInsights } from '../common/analytics';
 
 export const run: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
+    const telemetryQueue = req.body.data || req.body;
+
     context.log('Processing telemetry log request');
 
-    if (!req.body || !req.body.data) {
+    if (!Array.isArray(telemetryQueue)) {
         context.res = {
             body: 'Invalid request',
             status: 400
         };
+        context.done();
+
+        return;
     }
 
     try {
-        for (const telemetry of req.body.data) {
+        for (const telemetry of telemetryQueue) {
             addToAppInsights(telemetry.data.name, telemetry.data.properties, telemetry.data.measurements, new Date(telemetry.time));
         }
 
